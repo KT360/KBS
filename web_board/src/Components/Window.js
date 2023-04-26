5461151a021cad44ddb0c5d5020a58657bc30630
Janakiram <gjanakir@txt.textron.com>
2023-04-26 13:48:47 -0400

    First push

---
+++ web_board/src/Components/Window.js
@@ -0,0 +1,12 @@
+import useState from 'react'
+
+//Main window that manages each page, as a list and displays them 
+export default function Window()
+{
+
+    const [curr_page,setPage] = useState(null); // set to home page
+
+    return(<div>
+        
+    </div>);
+}
